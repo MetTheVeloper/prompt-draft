d1284676e982270e4b81e3d6efecceb5e8bf0bbf
MetTheVeloper <metadolin@gmail.com>
2026-08-21 15:09:38 +0330

    refactor(hair): add semantic hairstyle locale patch

---
+++ i18n/locales/hair.en.ts
@@ -0,0 +1,5 @@
+export default {
+  title: "Hair",
+  description:
+    "Build subject-scoped hairstyles from structural hair properties and optional components. Assign hair color and material from their dedicated modules.",
+};
