358c4671ca5db84d47ca741f472fcc838f0b31f7
MetTheVeloper <metadolin@gmail.com>
2026-08-22 12:33:16 +0330

    chore: remove duplicate commaSep auto-import

--- app/utils/format.ts
+++
@@ -1,9 +0,0 @@
-export const commaSep = (number: number | string | undefined | null) => {
-  if (number === undefined || number === null) {
-    return ''
-  }
-
-  return number
-    .toString()
-    .replace(/\B(?=(\d{3})+(?!\d))/g, ',')
-}
