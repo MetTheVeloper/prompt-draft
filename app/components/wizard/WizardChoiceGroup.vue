e62ce03fb19cc477b1d69cbde22c98d4c5cf6021
MetTheVeloper <metadolin@gmail.com>
2026-08-28 16:19:05 +0330

    fix(wizard): render valid selectable choice cards

--- app/components/wizard/WizardChoiceGroup.vue
+++ app/components/wizard/WizardChoiceGroup.vue
@@ -12,16 +12,37 @@ const emit = defineEmits<{
 </script>
 
 <template>
-  <el-grid :cols="['1fr', '1fr 1fr']" :gap="10">
+  <el-grid fit="fit" min="220px" max="1fr" :gap="10" class="w100">
     <el-button
       v-for="option in props.options"
       :key="option.value"
-      :label="option.label"
-      :mode="props.modelValue === option.value ? 'solid' : 'outline'"
+      :mode="props.modelValue === option.value ? 'normal' : 'outline'"
       color="blue"
       rules="rsc"
       :p="[12, 14]"
-      @click="emit('update:modelValue', option.value)"
-    />
+      @click="emit('update:modelValue', option.value)">
+      <el-flex rules="rsc" :gap="10" class="w100">
+        <el-icon
+          v-if="option.icon"
+          :icon="option.icon"
+          :size="18"
+          :color="props.modelValue === option.value ? 'white' : 'blue'"
+        />
+        <el-grid :gap="3">
+          <el-text
+            :size="13"
+            :weight="700"
+            :color="props.modelValue === option.value ? 'white' : 'blue'">
+            {{ option.label }}
+          </el-text>
+          <el-text
+            v-if="option.description"
+            :size="10"
+            :color="props.modelValue === option.value ? 'white' : 'normal50'">
+            {{ option.description }}
+          </el-text>
+        </el-grid>
+      </el-flex>
+    </el-button>
   </el-grid>
 </template>
