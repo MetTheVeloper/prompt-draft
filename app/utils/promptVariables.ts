0841ce18e56762a2519a16e2c59149807aa562c8
MetTheVeloper <metadolin@gmail.com>
2026-09-01 17:43:05 +0330

    refactor: release obsolete layout alias namespace

--- app/utils/promptVariables.ts
+++ app/utils/promptVariables.ts
@@ -11,7 +11,6 @@ export const RESERVED_VARIABLE_KEY_PATTERNS = [
   /^text_group_[a-z0-9_]*$/,
   /^outfit_[a-z0-9_]+$/,
   /^hair_[a-z0-9_]+$/,
-  /^r_[0-9]+$/,
   /^tg_[0-9]+$/,
   /^tt_[0-9]+$/,
 ];
