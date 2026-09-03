0932743a1abe09924032203e1151fa03d9f63748
MetTheVeloper <metadolin@gmail.com>
2026-09-03 16:00:42 +0330

    Remove unused Wizard backend locale fragment

--- i18n/locales/wizard-backend.en.ts
+++
@@ -1,8 +0,0 @@
-export default {
-  living: {
-    errors: {
-      persistence:
-        "Your prompt was generated, but this run could not be saved to history. You can keep using the finished prompt.",
-    },
-  },
-};
