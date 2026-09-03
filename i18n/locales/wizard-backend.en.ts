ccfbdebaf55c599586699ed4637069ffbd992d54
MetTheVeloper <metadolin@gmail.com>
2026-09-03 15:56:36 +0330

    Add Wizard persistence warning translation

---
+++ i18n/locales/wizard-backend.en.ts
@@ -0,0 +1,8 @@
+export default {
+  living: {
+    errors: {
+      persistence:
+        "Your prompt was generated, but this run could not be saved to history. You can keep using the finished prompt.",
+    },
+  },
+};
