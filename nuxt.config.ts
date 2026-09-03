af5e3fe72445f6ee74471569020ec9023d64c14b
MetTheVeloper <metadolin@gmail.com>
2026-09-03 15:44:13 +0330

    Add public API base runtime config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -10,6 +10,11 @@ export default defineNuxtConfig({
   experimental: {
     spaLoadingTemplateLocation: 'body',
   },
+  runtimeConfig: {
+    public: {
+      apiBase: process.env.NUXT_PUBLIC_API_BASE || "http://127.0.0.1:4000",
+    },
+  },
   nitro: {
     prerender: {
       routes: publicWizardRoutes,
