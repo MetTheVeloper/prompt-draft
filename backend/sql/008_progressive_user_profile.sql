25cf3655980b18d0c817b2e1558baf0ca8215533
MetTheVeloper <metadolin@gmail.com>
2026-09-04 13:24:24 +0330

    Harden progressive identity migration

--- backend/sql/008_progressive_user_profile.sql
+++ backend/sql/008_progressive_user_profile.sql
@@ -1,12 +1,30 @@
-ALTER TABLE users
-  DROP CONSTRAINT IF EXISTS users_check;
+DO $$
+DECLARE
+  constraint_row RECORD;
+BEGIN
+  FOR constraint_row IN
+    SELECT conname
+    FROM pg_constraint
+    WHERE conrelid = 'users'::regclass
+      AND contype = 'c'
+      AND conname <> 'users_identity_present_check'
+      AND pg_get_constraintdef(oid) ILIKE '%username%'
+      AND pg_get_constraintdef(oid) ILIKE '%email%'
+  LOOP
+    EXECUTE format(
+      'ALTER TABLE users DROP CONSTRAINT %I',
+      constraint_row.conname
+    );
+  END LOOP;
+END $$;
 
 DO $$
 BEGIN
   IF NOT EXISTS (
     SELECT 1
     FROM pg_constraint
-    WHERE conname = 'users_identity_present_check'
+    WHERE conrelid = 'users'::regclass
+      AND conname = 'users_identity_present_check'
   ) THEN
     ALTER TABLE users
       ADD CONSTRAINT users_identity_present_check
