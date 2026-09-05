382b1a4772d97317113004b339a6a6766bb90123
MetTheVeloper <metadolin@gmail.com>
2026-09-05 19:48:09 +0330

    Redesign Prompt Archive showcase cards

--- app/components/prompts/PromptItem.vue
+++ app/components/prompts/PromptItem.vue
@@ -21,14 +21,19 @@ const emit = defineEmits<{
 const auth = useAuth()
 const { t, locale } = useI18n()
 const { mobile } = useScreen()
+const hovered = ref(false)
 
 const isGrid = computed(() => props.view === 'grid')
 const canManageArchive = computed(() => auth.can(AUTH_PERMISSIONS.ARCHIVE_MANAGE))
 const hasTelegram = computed(() => Boolean(props.item.telegramUrl))
 
-const coverImage = computed(() => {
+const primaryImage = computed(() => {
   return props.item.coverImage?.thumbnailUrl || props.item.coverImage?.fullUrl || ''
 })
+const secondaryImage = computed(() => {
+  return props.item.secondaryImage?.thumbnailUrl || props.item.secondaryImage?.fullUrl || ''
+})
+const hasSecondaryImage = computed(() => Boolean(secondaryImage.value))
 const detailUrl = computed(() => `/prompts?id=${props.item.id}`)
 const manageEditUrl = computed(() => `/manage/archive?edit=${props.item.id}`)
 const localizedTitle = computed(() => {
@@ -36,7 +41,7 @@ const localizedTitle = computed(() => {
 })
 
 const visibleTagLimit = computed(() => {
-  if (isGrid.value) return 3
+  if (isGrid.value) return mobile.value ? 2 : 3
   return mobile.value ? 2 : 4
 })
 
@@ -72,146 +77,21 @@ const formattedDate = computed(() => {
   ).format(date)
 })
 
-const rootAttrs = computed(() => {
-  if (isGrid.value) {
-    return {
-      rules: 'cbs',
-      gap: 0,
-      radius: 16,
-      br: 1,
-      bc: 'normal10',
-      bg: 'surface',
-      class: 'prompt-item prompt-item--grid w100 h100',
-    }
-  }
+const listRootAttrs = computed(() => ({
+  rules: 'rbc',
+  gap: mobile.value ? 8 : 12,
+  p: mobile.value ? 7 : 9,
+  radius: 14,
+  br: 1,
+  bc: 'normal10',
+  bg: 'surface',
+  class: 'prompt-item prompt-item--list w100 ofh',
+}))
 
-  return {
-    rules: 'rbc',
-    gap: mobile.value ? 8 : 12,
-    p: mobile.value ? 7 : 9,
-    radius: 14,
-    br: 1,
-    bc: 'normal10',
-    bg: 'surface',
-    class: 'prompt-item prompt-item--list w100 ofh',
-  }
-})
-
-const mediaAttrs = computed(() => {
-  if (isGrid.value) {
-    return {
-      class: 'prompt-item__media prompt-item__media--grid w100 por ofh',
-    }
-  }
-
-  return {
-    class: 'prompt-item__media prompt-item__media--list ofh',
-    style: {
-      width: `${mobile.value ? 78 : 112}px`,
-      height: `${mobile.value ? 78 : 84}px`,
-    },
-  }
-})
-
-const bodyAttrs = computed(() => {
-  if (isGrid.value) {
-    return {
-      rules: 'cbs',
-      gap: 14,
-      p: 16,
-      class: 'prompt-item__body fg100 w100',
-    }
-  }
-
-  return {
-    rules: 'csc',
-    gap: mobile.value ? 6 : 8,
-    class: 'prompt-item__body fg100',
-  }
-})
-
-const titleAttrs = computed(() => {
-  return {
-    type: isGrid.value ? 'h2' : 'h3',
-    size: isGrid.value ? 16 : mobile.value ? 12 : 14,
-    weight: 800,
-    class: 'prompt-item__title w100',
-  }
-})
-
-const tagsAttrs = computed(() => {
-  return {
-    rules: 'rsc',
-    gap: 8,
-    wrap: true,
-    class: 'w100',
-  }
-})
-
-const tagAttrs = computed(() => {
-  return {
-    size: isGrid.value ? 10 : 9,
-    p: 4,
-    radius: 100,
-    marker: 'normal5',
-    class: 'wsnw',
-  }
-})
-
-const actionsAttrs = computed(() => {
-  return {
-    rules: isGrid.value
-      ? 'rbc'
-      : mobile.value
-        ? 'ccc'
-        : 'rcc',
-    gap: isGrid.value ? 6 : 5,
-    class: isGrid.value
-      ? 'prompt-item__actions w100'
-      : 'prompt-item__actions',
-  }
-})
-
-const telegramButtonAttrs = computed(() => {
-  return {
-    class: isGrid.value ? 'fg100' : undefined,
-    type: isGrid.value ? 'normal' : 'fab',
-    label: t('prompts.actions.telegram'),
-    icon: 'send',
-    color: 'blue',
-    size: 12,
-    p: isGrid.value ? [10, 12] : mobile.value ? 8 : 9,
-  }
-})
-
-const viewButtonAttrs = computed(() => {
-  const fillGridSpace = isGrid.value && !hasTelegram.value
-
-  return {
-    class: fillGridSpace ? 'fg100' : undefined,
-    type: fillGridSpace ? 'normal' : 'fab',
-    to: detailUrl.value,
-    label: t('prompts.actions.view'),
-    icon: 'visibility',
-    mode: 'flat',
-    color: 'normal',
-    size: 12,
-    p: fillGridSpace ? [10, 12] : isGrid.value ? 10 : mobile.value ? 8 : 9,
-  }
-})
-
-const editButtonAttrs = computed(() => {
-  return {
-    type: 'fab',
-    to: manageEditUrl.value,
-    label: t('manage.archive.actions.edit'),
-    icon: 'edit',
-    mode: 'flat',
-    color: 'normal',
-    size: 12,
-    p: isGrid.value ? 10 : mobile.value ? 8 : 9,
-  }
-})
+const listMediaStyle = computed(() => ({
+  width: `${mobile.value ? 78 : 112}px`,
+  height: `${mobile.value ? 78 : 84}px`,
+}))
 
 function formatTag(tag: string) {
   return tag.replaceAll('-', ' ')
@@ -224,140 +104,245 @@ function openTelegram() {
 </script>
 
 <template>
-  <el-flex v-bind="rootAttrs">
-    <div v-bind="mediaAttrs">
+  <el-flex
+    v-if="isGrid"
+    rules="ccs"
+    class="prompt-item prompt-item--grid w100"
+    :class="{ 'has-secondary': hasSecondaryImage }"
+    :gap="0"
+    :radius="18"
+    :br="1"
+    :bc="hovered ? 'normal50' : 'normal15'"
+    :effect="{ color: 'normal15' }"
+    @mouseenter="hovered = true"
+    @mouseleave="hovered = false">
+    <template v-if="primaryImage">
       <img
-        v-if="coverImage"
-        :src="coverImage"
+        :src="primaryImage"
+        :alt="localizedTitle"
+        class="prompt-item__bg prompt-item__bg--primary"
+        loading="lazy"
+        decoding="async"
+        draggable="false"
+      />
+      <img
+        v-if="secondaryImage"
+        :src="secondaryImage"
+        :alt="localizedTitle"
+        class="prompt-item__bg prompt-item__bg--secondary"
+        loading="lazy"
+        decoding="async"
+        draggable="false"
+      />
+    </template>
+    <div v-else class="prompt-item__bg-fallback" />
+    <div class="prompt-item__shade pen" />
+
+    <el-flex
+      rules="rbc"
+      class="prompt-item__topbar w100"
+      :gap="8"
+      bd="b8"
+      :radius="100"
+      bg="surface10">
+      <el-flex rules="rsc" class="fg100" :gap="8">
+        <el-button
+          type="fab"
+          :to="detailUrl"
+          :label="t('prompts.actions.view')"
+          icon="visibility"
+          mode="flat"
+          color="normal"
+          :tooltip="t('prompts.actions.view')"
+        />
+        <el-button
+          v-if="hasTelegram"
+          type="fab"
+          :label="t('prompts.actions.telegram')"
+          icon="send"
+          mode="flat"
+          color="blue"
+          :tooltip="t('prompts.actions.telegram')"
+          @click="openTelegram"
+        />
+        <el-button
+          v-if="canManageArchive"
+          type="fab"
+          :to="manageEditUrl"
+          :label="t('manage.archive.actions.edit')"
+          icon="edit"
+          mode="flat"
+          color="normal"
+          :tooltip="t('manage.archive.actions.edit')"
+        />
+      </el-flex>
+
+      <el-flex
+        v-if="item.imageCount"
+        rules="rcc"
+        class="prompt-item__media-count"
+        :p="[5, 8]"
+        :radius="100"
+        bg="surface75"
+        bd="b6">
+        <el-text :size="10" :weight="800">
+          {{ t('prompts.card.imageCount', { count: item.imageCount }) }}
+        </el-text>
+      </el-flex>
+    </el-flex>
+
+    <el-flex rules="ccs" class="prompt-item__content w100" :gap="12">
+      <el-flex rules="rsc" :gap="8" wrap class="w100 fw">
+        <el-text
+          :size="11"
+          :weight="800"
+          :marker="modelMarker"
+          color="white"
+          :p="[4, 7]"
+          :radius="100"
+          class="wsnw">
+          {{ modelLabel }}
+        </el-text>
+        <el-text
+          :size="11"
+          :weight="800"
+          marker="surface"
+          color="normal"
+          :p="[4, 7]"
+          :radius="100"
+          class="wsnw">
+          #{{ item.id }}
+        </el-text>
+      </el-flex>
+
+      <el-flex v-if="item.tags.length" rules="rsc" :gap="7" wrap class="w100 fw prompt-item__tags">
+        <el-text
+          v-for="tag in visibleTags"
+          :key="tag"
+          :size="10"
+          marker="surface75"
+          color="white"
+          :p="[3, 6]"
+          :radius="100"
+          class="wsnw">
+          {{ formatTag(tag) }}
+        </el-text>
+        <el-text
+          v-if="hiddenTagCount"
+          :size="10"
+          marker="surface75"
+          color="white"
+          :p="[3, 6]"
+          :radius="100"
+          class="wsnw">
+          +{{ hiddenTagCount }}
+        </el-text>
+      </el-flex>
+
+      <el-text
+        type="h3"
+        :size="mobile ? 38 : 46"
+        :weight="650"
+        color="white"
+        class="prompt-item__title">
+        {{ localizedTitle }}
+      </el-text>
+
+      <el-flex rules="rsc" :gap="12" wrap class="prompt-item__meta w100 fw">
+        <el-text
+          :size="mobile ? 11 : 12"
+          color="white"
+          icon="calendar_month"
+          icon-color="white">
+          {{ formattedDate }}
+        </el-text>
+        <el-text
+          v-if="item.imageCount"
+          :size="mobile ? 11 : 12"
+          color="white"
+          icon="photo_library"
+          icon-color="white">
+          {{ t('prompts.card.imageCount', { count: item.imageCount }) }}
+        </el-text>
+      </el-flex>
+    </el-flex>
+  </el-flex>
+
+  <el-flex v-else v-bind="listRootAttrs">
+    <div class="prompt-item__media prompt-item__media--list ofh" :style="listMediaStyle">
+      <img
+        v-if="primaryImage"
+        :src="primaryImage"
         :alt="localizedTitle"
         class="prompt-item__image w100 h100"
         loading="lazy"
         decoding="async"
       />
-
-      <el-flex
-        v-else
-        rules="ccc"
-        class="w100 h100"
-        bg="normal5">
-        <el-icon
-          icon="hide_image"
-          :size="isGrid ? 42 : mobile ? 22 : 28"
-          color="normal30"
-        />
-
-        <el-text
-          v-if="isGrid"
-          :size="11"
-          color="normal40">
-          {{ t('prompts.card.noPreview') }}
-        </el-text>
-      </el-flex>
-
-      <el-flex
-        v-if="isGrid"
-        rules="rbc"
-        class="poa t0 l0 r0"
-        :p="10"
-        :gap="6">
-        <el-text
-          :size="10"
-          :weight="700"
-          :p="[0, 2]"
-          :radius="100"
-          :marker="modelMarker"
-          class="wsnw">
-          {{ modelLabel }}
-        </el-text>
-
-        <el-text
-          :size="10"
-          :weight="700"
-          :p="[0, 2]"
-          :radius="100"
-          marker="white"
-          color="blue"
-          class="wsnw">
-          #{{ item.id }}
-        </el-text>
+      <el-flex v-else rules="ccc" class="w100 h100" bg="normal5">
+        <el-icon icon="hide_image" :size="mobile ? 22 : 28" color="normal30" />
       </el-flex>
     </div>
 
-    <el-flex v-bind="bodyAttrs">
-      <el-grid
-        v-if="isGrid"
-        :gap="7"
-        class="w100">
-        <el-text v-bind="titleAttrs">
-          {{ localizedTitle }}
-        </el-text>
-
-        <el-flex rules="rsc" :gap="8" wrap>
-          <el-text
-            :size="11"
-            color="normal50"
-            icon="calendar_month">
-            {{ formattedDate }}
-          </el-text>
-
-          <el-text
-            v-if="item.imageCount"
-            :size="11"
-            color="normal50"
-            icon="photo_library">
-            {{ t('prompts.card.imageCount', { count: item.imageCount }) }}
-          </el-text>
-        </el-flex>
-      </el-grid>
-
-      <el-text
-        v-else
-        v-bind="titleAttrs">
+    <el-flex rules="csc" :gap="mobile ? 6 : 8" class="prompt-item__body fg100">
+      <el-text type="h3" :size="mobile ? 12 : 14" :weight="800" class="prompt-item__list-title w100">
         {{ localizedTitle }}
       </el-text>
 
-      <el-flex
-        v-if="item.tags.length"
-        v-bind="tagsAttrs">
+      <el-flex v-if="item.tags.length" rules="rsc" :gap="8" wrap class="w100">
         <el-text
           v-for="tag in visibleTags"
           :key="tag"
-          v-bind="tagAttrs">
+          :size="9"
+          :p="4"
+          :radius="100"
+          marker="normal5"
+          class="wsnw">
           {{ formatTag(tag) }}
         </el-text>
-
         <el-text
           v-if="hiddenTagCount"
-          v-bind="tagAttrs">
+          :size="9"
+          :p="4"
+          :radius="100"
+          marker="normal5"
+          class="wsnw">
           +{{ hiddenTagCount }}
         </el-text>
       </el-flex>
-
-      <el-flex
-        v-if="isGrid"
-        v-bind="actionsAttrs">
-        <el-button
-          v-if="hasTelegram"
-          v-bind="telegramButtonAttrs"
-          @click="openTelegram"
-        />
-
-        <el-button v-bind="viewButtonAttrs" />
-        <el-button v-if="canManageArchive" v-bind="editButtonAttrs" />
-      </el-flex>
     </el-flex>
 
-    <el-flex
-      v-if="!isGrid"
-      v-bind="actionsAttrs">
+    <el-flex :rules="mobile ? 'ccc' : 'rcc'" :gap="5" class="prompt-item__actions">
       <el-button
         v-if="hasTelegram"
-        v-bind="telegramButtonAttrs"
+        type="fab"
+        :label="t('prompts.actions.telegram')"
+        icon="send"
+        color="blue"
+        :size="12"
+        :p="mobile ? 8 : 9"
         @click="openTelegram"
       />
-
-      <el-button v-bind="viewButtonAttrs" />
-      <el-button v-if="canManageArchive" v-bind="editButtonAttrs" />
+      <el-button
+        type="fab"
+        :to="detailUrl"
+        :label="t('prompts.actions.view')"
+        icon="visibility"
+        mode="flat"
+        color="normal"
+        :size="12"
+        :p="mobile ? 8 : 9"
+      />
+      <el-button
+        v-if="canManageArchive"
+        type="fab"
+        :to="manageEditUrl"
+        :label="t('manage.archive.actions.edit')"
+        icon="edit"
+        mode="flat"
+        color="normal"
+        :size="12"
+        :p="mobile ? 8 : 9"
+      />
     </el-flex>
   </el-flex>
 </template>
@@ -368,15 +353,108 @@ function openTelegram() {
   overflow: hidden;
 }
 
+.prompt-item--grid {
+  position: relative;
+  aspect-ratio: 1 / 1;
+  isolation: isolate;
+  background: var(--themeBackground);
+  box-shadow: 0 18px 55px rgba(0, 0, 0, 0.22);
+  transition: transform 260ms ease, border-color 220ms ease, box-shadow 260ms ease;
+}
+
+.prompt-item--grid:hover {
+  transform: translateY(-4px);
+  box-shadow: 0 24px 70px rgba(0, 0, 0, 0.28);
+}
+
+.prompt-item--grid :deep(.effect) {
+  z-index: 4;
+  mix-blend-mode: soft-light;
+}
+
+.prompt-item__bg,
+.prompt-item__bg-fallback,
+.prompt-item__shade {
+  position: absolute;
+  inset: 0;
+  width: 100%;
+  height: 100%;
+}
+
+.prompt-item__bg {
+  z-index: 1;
+  display: block;
+  object-fit: cover;
+  object-position: center;
+  user-select: none;
+  pointer-events: none;
+  transition: opacity 420ms ease, transform 700ms cubic-bezier(.2, .7, .2, 1);
+}
+
+.prompt-item__bg--primary { opacity: 1; }
+.prompt-item__bg--secondary { opacity: 0; transform: scale(1.035); }
+.prompt-item--grid:hover .prompt-item__bg { transform: scale(1.025); }
+.prompt-item--grid.has-secondary:hover .prompt-item__bg--primary { opacity: 0; }
+.prompt-item--grid.has-secondary:hover .prompt-item__bg--secondary { opacity: 1; transform: scale(1.025); }
+
+.prompt-item__bg-fallback {
+  z-index: 1;
+  background:
+    radial-gradient(circle at 18% 18%, var(--primary35), transparent 42%),
+    radial-gradient(circle at 82% 22%, var(--themePurple25), transparent 46%),
+    linear-gradient(145deg, var(--themeSurface20), var(--themeBackground));
+}
+
+.prompt-item__shade {
+  z-index: 2;
+  background:
+    linear-gradient(180deg, rgba(0, 0, 0, 0.18) 0%, transparent 30%, rgba(0, 0, 0, 0.08) 48%, rgba(0, 0, 0, 0.86) 100%),
+    linear-gradient(90deg, rgba(0, 0, 0, 0.18) 0%, transparent 54%);
+}
+
+.prompt-item__topbar {
+  position: absolute;
+  inset-block-start: 0;
+  inset-inline-start: 0;
+  z-index: 6;
+  padding: 14px;
+}
+
+.prompt-item__media-count {
+  flex: 0 0 auto;
+  box-shadow: 0 8px 24px rgba(0, 0, 0, 0.18);
+}
+
+.prompt-item__content {
+  position: absolute;
+  inset-inline-start: 0;
+  inset-block-end: 0;
+  z-index: 6;
+  padding: clamp(18px, 4vw, 26px);
+}
+
+.prompt-item__title {
+  width: 100%;
+  line-height: 1.02 !important;
+  letter-spacing: -0.035em;
+  text-wrap: balance;
+  text-shadow: 0 6px 24px rgba(0, 0, 0, 0.42);
+  display: -webkit-box;
+  -webkit-box-orient: vertical;
+  -webkit-line-clamp: 3;
+  overflow: hidden;
+}
+
+.prompt-item__tags,
+.prompt-item__meta {
+  opacity: 0.88;
+  text-shadow: 0 3px 14px rgba(0, 0, 0, 0.55);
+}
+
 .prompt-item__media {
   flex: 0 0 auto;
   overflow: hidden;
   background: var(--normal5, transparent);
-}
-
-.prompt-item__media--grid {
-  aspect-ratio: 4 / 3;
-  min-height: 180px;
 }
 
 .prompt-item__media--list {
@@ -388,29 +466,16 @@ function openTelegram() {
   object-fit: cover;
 }
 
-.prompt-item--grid .prompt-item__image {
-  transition: transform 450ms ease;
-  will-change: transform;
-}
-
-.prompt-item--grid:hover .prompt-item__image {
-  transform: scale(1.025);
-}
-
 .prompt-item__body {
   min-width: 0;
 }
 
-.prompt-item__title {
+.prompt-item__list-title {
   display: -webkit-box;
   overflow: hidden;
   -webkit-box-orient: vertical;
   -webkit-line-clamp: 2;
   line-clamp: 2;
-}
-
-.prompt-item--grid .prompt-item__title {
-  min-height: 2.55em;
 }
 
 .prompt-item__actions {
