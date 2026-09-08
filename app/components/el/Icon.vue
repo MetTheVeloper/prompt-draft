227e37999e08338bcbf8f8a287cf22dbafe47ab1
MetTheVeloper <metadolin@gmail.com>
2026-09-08 21:00:57 +0330

    chore: keep back icon fix scoped to prompt detail

--- app/components/el/Icon.vue
+++ app/components/el/Icon.vue
@@ -7,7 +7,7 @@
     <span
       :class="symbolClass"
       :style="symbolStyle"
-    >{{ resolvedIcon }}</span>
+    >{{ icon }}</span>
 
     <span
       v-if="badge"
@@ -53,12 +53,6 @@ function clamp(value: number, min: number, max: number) {
   return Math.min(max, Math.max(min, value));
 }
 
-const legacyIconAliases: Record<string, string> = {
-  "arrow-left": "arrow_back",
-  "arrow-right": "arrow_forward",
-};
-
-const resolvedIcon = computed(() => legacyIconAliases[props.icon] || props.icon);
 const sizes = computed(() => dimension(props.size || app.settings.globalSize));
 const iconSize = computed(() => props.size ?? sizes.value.icon);
 const symbolWeight = computed(() => clamp(props.weight, 100, 700));
