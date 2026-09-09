a8993111dd95280e696d5d4ed2af3b8ed53a283e
MetTheVeloper <metadolin@gmail.com>
2026-09-09 10:49:10 +0330

    feat: mirror Creator review permission in frontend

--- app/config/authorization.ts
+++ app/config/authorization.ts
@@ -3,6 +3,7 @@ export const AUTH_PERMISSIONS = {
   SYSTEM_METRICS_VIEW: "system.metrics.view",
   USERS_VIEW: "users.view",
   USERS_MANAGE: "users.manage",
+  CREATORS_MANAGE: "creators.manage",
   DRAFTS_VIEW_ALL: "drafts.view_all",
   DRAFTS_DELETE_ANY: "drafts.delete_any",
   SYSTEM_SETTINGS_MANAGE: "system.settings.manage",
