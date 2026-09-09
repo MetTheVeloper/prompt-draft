0e221a1c7a713e124634584e93fecc019e76a3e4
MetTheVeloper <metadolin@gmail.com>
2026-09-09 13:26:15 +0330

    fix: render editable email beside username

--- app/pages/manage/profile.vue
+++ app/pages/manage/profile.vue
@@ -478,7 +478,7 @@ onBeforeUnmount(() => {
           </el-text>
         </el-flex>
 
-        <div class="profile-account-grid w100">
+        <el-grid cols="minmax(240px, 1fr) minmax(240px, 1fr)" :gap="12" class="w100">
           <el-flex rules="ccs" :gap="6" dir="ltr">
             <el-text :size="11" :weight="700">{{ t("manage.profile.account.username") }}</el-text>
             <el-text-field
@@ -498,13 +498,14 @@ onBeforeUnmount(() => {
               v-model="form.email"
               type="text"
               inputmode="email"
+              autocomplete="email"
               :actions="false"
               :disabled="saving"
               :placeholder="t('manage.profile.account.emailPlaceholder')"
             />
             <el-text :size="10" color="normal55">{{ t("manage.profile.account.emailPrivate") }}</el-text>
           </el-flex>
-        </div>
+        </el-grid>
       </el-flex>
 
       <el-flex rules="csc" :gap="14" :p="18" bg="surface" :radius="16" :br="1" bc="normal15" class="w100">
@@ -659,12 +660,6 @@ onBeforeUnmount(() => {
   max-width: 1120px;
 }
 
-.profile-account-grid {
-  display: grid;
-  grid-template-columns: repeat(2, minmax(240px, 1fr));
-  gap: 12px;
-}
-
 .profile-hidden-input {
   position: absolute;
   width: 1px;
@@ -719,11 +714,5 @@ onBeforeUnmount(() => {
   z-index: 4;
   transform: translateY(50%);
   filter: drop-shadow(0 10px 24px rgba(0, 0, 0, 0.3));
-}
-
-@media (max-width: 720px) {
-  .profile-account-grid {
-    grid-template-columns: 1fr;
-  }
 }
 </style>
