162343a3d4c169ae65c021890a3bab5667e99351
MetTheVeloper <metadolin@gmail.com>
2026-09-10 19:00:53 +0330

    feat: add reusable zoomable Blog image

---
+++ app/components/blog/BlogZoomableImage.vue
@@ -0,0 +1,46 @@
+<script setup lang="ts">
+const props = withDefaults(defineProps<{
+  src: string
+  alt?: string
+  width?: number | null
+  height?: number | null
+  loading?: 'lazy' | 'eager'
+}>(), {
+  alt: '',
+  width: null,
+  height: null,
+  loading: 'lazy',
+})
+
+const lightbox = useBlogImageLightbox()
+
+function openImage() {
+  lightbox.open({
+    src: props.src,
+    alt: props.alt,
+  })
+}
+</script>
+
+<template>
+  <img
+    :src="src"
+    :alt="alt"
+    :width="width || undefined"
+    :height="height || undefined"
+    :loading="loading"
+    decoding="async"
+    role="button"
+    tabindex="0"
+    class="blog-zoomable-image"
+    @click="openImage"
+    @keydown.enter.prevent="openImage"
+    @keydown.space.prevent="openImage"
+  >
+</template>
+
+<style scoped>
+.blog-zoomable-image {
+  cursor: zoom-in;
+}
+</style>
