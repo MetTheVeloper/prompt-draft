2d5b610e383dfe8abd2a73a3e7eededa9ef2c17d
MetTheVeloper <metadolin@gmail.com>
2026-09-10 14:48:40 +0330

    feat: read Blog management index from canonical Git when configured

--- server/api/manage/blog/index.get.ts
+++ server/api/manage/blog/index.get.ts
@@ -1,15 +1,17 @@
 import { projectManageBlogArticleSummary } from '../../../../shared/manage-blog'
-import { loadBlogRepository } from '../../../utils/blogRepository'
 import { requireBlogManage } from '../../../utils/blogManageAuthorization'
+import { getBlogManageRepository } from '../../../utils/blogManageRepository'
 
 export default defineEventHandler(async (event) => {
   await requireBlogManage(event)
   setHeader(event, 'Cache-Control', 'no-store')
 
-  const articles = await loadBlogRepository()
+  const repository = await getBlogManageRepository(event)
   return {
     ok: true,
-    articles: articles
+    repositorySource: repository.source,
+    writeConfigured: repository.writeConfigured,
+    articles: repository.articles
       .map(projectManageBlogArticleSummary)
       .sort((left, right) => {
         const updatedOrder = right.updatedAt.localeCompare(left.updatedAt)
