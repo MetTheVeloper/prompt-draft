10655092b40bdfc7e39d5be8f0f9ddcc5092a463
MetTheVeloper <metadolin@gmail.com>
2026-09-14 09:46:33 +0330

    fix: register Archive Telegram adapter facade

---
+++ app/components/ArchiveTelegramAdapter.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+import ManageArchiveTelegramAdapter from "~/components/manage/ArchiveTelegramAdapter.vue";
+import type { AdminArchiveItem } from "~/types/adminArchiveApi";
+
+const props = defineProps<{
+  item: AdminArchiveItem;
+  disable?: boolean;
+}>();
+</script>
+
+<template>
+  <ManageArchiveTelegramAdapter :item="props.item" />
+</template>
